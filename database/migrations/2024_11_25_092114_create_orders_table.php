dce2c2a0801c36dd2c66123207bf2958bfdd7fe7
duongnguyen <duongnt.ml08@gmail.com>
2024-11-25 16:33:06 +0700

    FP: Create entity Order and OrderItem

--- database/migrations/2024_11_25_092114_create_orders_table.php
+++ database/migrations/2024_11_25_092114_create_orders_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('invoice_id');
+            $table->foreignId('user_id')->constrained();
+            $table->text('address');
+            $table->double('discount')->default(0);
+            $table->double('delivery_charge')->default(0);
+            $table->double('subtotal');
+            $table->double('grand_total');
+            $table->integer('product_qty');
+            $table->string('payment_method')->nullable();
+            $table->string('payment_status')->default('pending');
+            $table->timestamp('payment_approve_date')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->json('coupon_info')->nullable();
+            $table->string('currency_name');
+            $table->string('order_status')->default('pending');
             $table->timestamps();
         });
     }
