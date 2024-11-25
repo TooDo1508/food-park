83ba46d6f73f18934c00c2eae361fbe35e306405
duongnguyen <duongnt.ml08@gmail.com>
2024-11-25 09:46:22 +0700

    FP: Show/hide cart address

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\AddressCreateRequest;
+use App\Models\Address;
+use App\Models\DeliveryArea;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -10,6 +13,25 @@ class DashboardController extends Controller
 {
     // //
     public function index() : View {
-        return view('frontend.dashboard.index');
+        $deliveryAreas = DeliveryArea::where('status', 1)->get();
+        $userAddress = Address::where('user_id', auth()->user()->id)->get();
+        return view('frontend.dashboard.index', compact('deliveryAreas', 'userAddress'));
+    }
+
+    public function createAddress(AddressCreateRequest $request){
+        $address = new Address();
+        $address->user_id = auth()->user()->id;
+        $address->delivery_area_id = $request->area;
+        $address->first_name = $request->first_name;
+        $address->last_name = $request->last_name;
+        $address->phone = $request->phone;
+        $address->email = $request->email;
+        $address->address = $request->address;
+        $address->type = $request->type;
+        $address->save();
+
+        toastr()->success('Created address successfully');
+
+        return to_route('dashboard');
     }
 }
